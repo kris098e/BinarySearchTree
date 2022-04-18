c87f763bbfe74eba9ca06c8d7acead03ff2b8006
Kristian Degn Abrahamsen <krabr21@student.sdu.dk>
2022-04-18 17:48:13 +0200

    commented

--- DictBinTree.java
+++ DictBinTree.java
@@ -9,24 +9,35 @@ import java.util.ArrayList;
 
 public class DictBinTree implements Dict {
 
+    // Pointer root, every method is done via the root-attribute
     private BinNode root;
 
+    // Method for searching whether the specific key, given in the parameter of the function, is contained in the tree at the current state of the tree
     public boolean search(int key) {
         return auxSearch(root, key);
     }
+
+    // Helper method for search. 
     private boolean auxSearch(BinNode node, int key) {
+        // If in a leaf (null node), the key isnt contained in the tree
         if(node == null)
             return false;
         if(node.key == key)
             return true;
+        // If key-value is less than current node-key-value, continue down left path to left child
         if(key < node.key)
             return auxSearch(node.left, key);
+        // Else go right.
         else return auxSearch(node.right, key);
     }
 
+    // Method for inserting a new key
     public void insert(int key) {
-        BinNode y = null;
-        BinNode x = root;
+        // Temp-helper nodes for updating pointers
+        BinNode y = null; 
+        BinNode x = root; 
+
+        // Looping down to a leaf
         while(x != null) {
             y = x;
             if(key < x.key) {
@@ -35,19 +46,21 @@ public class DictBinTree implements Dict {
                 x = x.right;
             }
         }
-        if(y == null)
+        if(y == null) // Case where root is null, we create the tree with the key
             root = new BinNode(key);
-        else if(key < y.key)
+        else if(key < y.key) // Case where key is less than parent-node, left child is created to be a new node with the key-data 
             y.left = new BinNode(key);
-        else y.right = new BinNode(key);
+        else y.right = new BinNode(key); // If not less or equal to parent-node, right child is created to be a new node with the key-data 
     }
 
+    // Method returning an ArrayList list object containing Integer object, where the Integer objects are sorted by an inorder-traversal
     public ArrayList<Integer> orderedTraversal() {
         ArrayList<Integer> list = new ArrayList<>();
         auxInorder(root, list);
         return list;
     }
 
+    // Helper method for adding elements to the ArrayList object given in the argument, in an inorder traversal recursivly
     private void auxInorder(BinNode node, ArrayList<Integer> list) {
         if(node != null) {
             auxInorder(node.left, list);
@@ -56,7 +69,7 @@ public class DictBinTree implements Dict {
         }
     }
 
-
+    // The node-class used for making the tree-structure
     private static class BinNode {
 
         private int key;
